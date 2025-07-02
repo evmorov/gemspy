9b20b93bcd7faa3bbbff8044d456c90a40a67eca
Evgenii Morozov <evmorov@gmail.com>
2025-07-03 01:12:41 +0200

    It seems to work

--- lib/gemspy.rb
+++ lib/gemspy.rb
@@ -3,28 +3,28 @@
 require_relative 'gemspy/version'
 
 require 'optparse'
+require 'csv'
 require 'active_support'
 require 'active_support/core_ext'
 
 module Gemspy
   class Cli
-    State = Struct.new(:gem_list_path, :apps_path, :gems, :app_dirs, keyword_init: true) do
-      def initialize(gem_list_path: nil, apps_path: nil, gems: [], app_dirs: [])
+    State = Struct.new(:gem_list_path, :apps_path, :output_csv, :apps, :gems, :scan, keyword_init: true) do
+      def initialize(gem_list_path: nil, apps_path: nil, output_csv: nil, apps: [], gems: [], scan: {})
         super
       end
     end
 
     def initialize
       @state = State.new
-      @state.gems = []
-      @state.app_dirs = []
     end
 
     def start
       parse_options
       validate_options
+      read_options
       spy_gems
-      puts @state
+      output_xls
     end
 
     private
@@ -40,6 +40,7 @@ module Gemspy
 
         parser.on('-g', '--gems FILE_PATH', 'Path to a list of gems to spy') { |g| @state.gem_list_path = g }
         parser.on('-a', '--apps DIRECTORY_PATH', 'Directory path with apps') { |a| @state.apps_path = a }
+        parser.on('-o', '--output FILENAME.csv', 'Ouput CSV filename') { |o| @state.output_csv = o }
         parser.on_tail('-v', '--version', 'Show version') do
           puts VERSION
           exit
@@ -55,18 +56,15 @@ module Gemspy
     def validate_options
       error '-g is required' if @state.gem_list_path.nil?
       error '-p is required' if @state.apps_path.nil?
+      error '-o is required' if @state.output_csv.nil?
 
       error "File '#{@state.gem_list_path}' isn't found" unless File.exist?(@state.gem_list_path)
       error "Directory '#{@state.apps_path}' isn't found" unless Dir.exist?(@state.apps_path)
     end
 
-    def spy_gems
-      @app_dirs = []
-
-      Dir.foreach(@state.apps_path) do |app_dir|
-        next if app_dir.start_with?('.')
-
-        @state.app_dirs << app_dir
+    def read_options
+      Dir.foreach(@state.apps_path) do |app|
+        @state.apps << app unless app.start_with?('.')
       end
 
       File.read(@state.gem_list_path).split("\n").each do |gem|
@@ -75,6 +73,58 @@ module Gemspy
 
         @state.gems << gem
       end
+
+      @state.gems.each do |gem|
+        @state.scan[gem] = {}
+      end
+    end
+
+    def spy_gems
+      @state.apps.each do |app|
+        lock = File.join(@state.apps_path, app, 'Gemfile.lock')
+        next unless File.exist?(lock)
+
+        File.open(lock).each_line do |line|
+          @state.gems.each do |gem|
+            version = fetch_version(line, gem)
+            @state.scan[gem][app] = version if version
+          end
+        end
+      end
+    end
+
+    def fetch_version(line, gem)
+      escaped_gem = Regexp.escape(gem)
+      match = line.strip.match(/^#{escaped_gem} \((\d+(?:\.\d+)*)\)$/)
+      return unless match
+
+      match[1]
+    end
+
+    def output_xls
+      headers = [''] + @state.gems
+
+      CSV.open(@state.output_csv, 'w', write_headers: true, headers:, col_sep: ';') do |csv|
+        apps_with_versions.each do |app|
+          line = [app]
+          @state.gems.each do |gem|
+            app_to_version = @state.scan[gem]
+            version = app_to_version ? app_to_version[app] : nil
+            line << version
+          end
+          csv << line
+        end
+      end
+    end
+
+    def apps_with_versions
+      apps_with_versions = []
+      @state.scan.each_value do |app_to_version|
+        app_to_version.each do |app, version|
+          apps_with_versions << app if version.present?
+        end
+      end
+      apps_with_versions.uniq.sort
     end
 
     def error(msg)
